910099a4716b0600f5002b117e7bde7d50906764
nightlaro <mickocabacungan@gmail.com>
2021-07-28 13:58:02 -0700

    Implement button to start DefaultColorsActivity

---
+++ app/src/main/java/com/example/colortaste/DefaultColorsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.colortaste
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DefaultColorsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_default_colors)
+    }
+}
--- app/src/main/java/com/example/colortaste/MainActivity.kt
+++ app/src/main/java/com/example/colortaste/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.colortaste
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val chooseButton = findViewById<Button>(R.id.choose_color_button)
+        chooseButton.setOnClickListener { _ ->
+            startDefaultColorsActivity()
+        }
+    }
+
+    fun startDefaultColorsActivity() {
+        val intent = Intent(this, DefaultColorsActivity::class.java)
+        startActivity(intent)
     }
 }
