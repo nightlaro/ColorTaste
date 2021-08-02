07717abb6e9123fe702a9edf0df0990fc4b88cc7
nightlaro <mickocabacungan@gmail.com>
2021-08-02 12:02:13 -0700

    Dialog flow WIP

--- app/src/main/java/com/example/colortaste/CustomColorActivity.kt
+++ app/src/main/java/com/example/colortaste/CustomColorActivity.kt
@@ -3,6 +3,7 @@ package com.example.colortaste
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class CustomColorActivity : AppCompatActivity() {
     lateinit var customColorRecyclerView: RecyclerView
@@ -11,5 +12,14 @@ class CustomColorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_custom_color)
+        val addColorButton : FloatingActionButton = findViewById(R.id.add_color_button)
+
+        addColorButton.setOnClickListener {
+            startDialog()
+        }
+    }
+
+    private fun startDialog() {
+        TODO("Not yet implemented")
     }
 }
