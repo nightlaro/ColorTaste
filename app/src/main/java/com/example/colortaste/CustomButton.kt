44429033fd03c44b6fc94f432e5e12b1629aa25e
nightlaro <mickocabacungan@gmail.com>
2021-08-03 13:59:42 -0700

    Removed Serializable type because.. I didn't need it

--- app/src/main/java/com/example/colortaste/CustomButton.kt
+++ app/src/main/java/com/example/colortaste/CustomButton.kt
@@ -1,7 +1,3 @@
 package com.example.colortaste
 
-import java.io.Serializable
-
-data class CustomButton(val title : String, val color: String) : Serializable {
-
-}
+data class CustomButton(val title : String, val color: String)
